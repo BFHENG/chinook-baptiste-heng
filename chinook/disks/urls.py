0e0f5977d968697bc43cda2f2d10df27864567d5
BFHENG <baptiste.heng.s@gmail.com>
2022-04-19 09:48:53 +0200

    liste détails album fontionnelle

--- chinook/disks/urls.py
+++ chinook/disks/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:album_id>/', views.detail_album, name='detail_album'),
 ]
--- chinook/disks/views.py
+++ chinook/disks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from .models import Album
@@ -13,5 +13,12 @@ def index(request):
     
     return HttpResponse(template.render(context, request))
     
+def detail_album (request, album_id):
+    #return HttpResponse("You're looking at album %s." % album_id)
+    
+    album = get_object_or_404(Album, pk=album_id)
+    
+    return render(request, 'disks/album_detail.html', {'album': album})
 
+    
 # Create your views here.
